79206f009343ba9c93fb06b3869b60c9dd8b6029
a233057 <meluleki.madlala@standardbank.co.za>
2020-05-04 03:08:11 +0200

    footer and sub-footer design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ExperiencesComponent } from './pages/experiences/experiences.component';
+import { SubFooterComponent } from './pages/sub-footer/sub-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ExperiencesComponent,
+    SubFooterComponent
   ],
   imports: [
     BrowserModule,
